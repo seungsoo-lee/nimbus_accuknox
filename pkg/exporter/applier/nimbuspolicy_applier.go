82abc9d1322a028ba5366309c0a0d47849f5b772
b0m313 <13.spring.03@gmail.com>
2024-01-12 01:58:42 +0000

    feat(applier): Add NimbusPolicy Watcher

--- pkg/exporter/applier/nimbuspolicy_applier.go
+++ pkg/exporter/applier/nimbuspolicy_applier.go
@@ -5,50 +5,68 @@ package applier
 
 import (
 	"context"
-	"fmt"
+	"time"
 
 	v1 "github.com/5GSEC/nimbus/api/v1"
+	"github.com/5GSEC/nimbus/pkg/exporter/nimbuspolicy"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 )
 
 // NimbusPolicyApplier is responsible for applying NimbusPolicy objects to the Kubernetes cluster.
 type NimbusPolicyApplier struct {
-	Client client.Client
+	Client       client.Client
+	Scheme       *runtime.Scheme
+	NimbusPolicy *nimbuspolicy.NimbusPolicyReconciler
 }
 
 // NewNimbusPolicyApplier creates a new instance of NimbusPolicyApplier.
-func NewNimbusPolicyApplier(client client.Client) *NimbusPolicyApplier {
+func NewNimbusPolicyApplier(client client.Client, scheme *runtime.Scheme) (*NimbusPolicyApplier, error) {
 	return &NimbusPolicyApplier{
-		Client: client,
-	}
+		Client:       client,
+		Scheme:       scheme,
+		NimbusPolicy: nimbuspolicy.NewNimbusPolicyReconciler(client, scheme),
+	}, nil
 }
 
-func (npa *NimbusPolicyApplier) ApplyNimbusPolicy(ctx context.Context, policy *v1.NimbusPolicy) error {
-	logger := log.FromContext(ctx)
+func (npa *NimbusPolicyApplier) Applier(ctx context.Context, req ctrl.Request, policy *v1.NimbusPolicy) (ctrl.Result, error) {
+	log := log.FromContext(ctx)
 
-	// Check if the NimbusPolicy already exists.
 	existingPolicy := &v1.NimbusPolicy{}
 	err := npa.Client.Get(ctx, client.ObjectKeyFromObject(policy), existingPolicy)
 
-	// Handle NotFound errors the right way
-	if err != nil {
-		if client.IgnoreNotFound(err) != nil {
-			return fmt.Errorf("Failed to check for existing NimbusPolicy: %v", err)
-		}
-		// If it's a NotFound error, create a new Nimbus policy.
-		logger.Info("Apply NimbusPolicy", "Policy", policy.Name)
+	if err != nil && !errors.IsNotFound(err) {
+		log.Error(err, "Failed to get NimbusPolicy", "Policy", policy.Name)
+		return ctrl.Result{}, err
+	}
+
+	if errors.IsNotFound(err) {
+		// If NimbusPolicy already exists, update it.
+		log.Info("Applying new NimbusPolicy", "Policy", policy.Name)
 		if err := npa.Client.Create(ctx, policy); err != nil {
-			return fmt.Errorf("Failed to Apply NimbusPolicy: %v", err)
+			log.Error(err, "Failed to apply NimbusPolicy", "Policy", policy.Name)
+			return ctrl.Result{}, err
 		}
 	} else {
-		// If the policy already exists, update it.
-		logger.Info("Update NimbusPolicy", "Policy", policy.Name)
+		// If NimbusPolicy already exists, update it.
+		log.Info("Updating existing NimbusPolicy", "Policy", policy.Name)
 		policy.ResourceVersion = existingPolicy.ResourceVersion
 		if err := npa.Client.Update(ctx, policy); err != nil {
-			return fmt.Errorf("Failed to update NimbusPolicy: %v", err)
+			log.Error(err, "Failed to update NimbusPolicy", "Policy", policy.Name)
+			return ctrl.Result{}, err
 		}
 	}
 
-	return nil
+	time.Sleep(time.Second * 5)
+
+	_, err = npa.NimbusPolicy.Reconcile(ctx, req)
+	if err != nil {
+		log.Error(err, "Failed to reconcile NimbusPolicy", "Policy", policy.Name)
+		return ctrl.Result{}, err
+	}
+
+	return ctrl.Result{}, nil
 }
